6c1f94fb56bf1b3cd37519713f1a32c2cf64c813
Marcos Oliveira Junqueira <2229824+marcosjunqueira@users.noreply.github.com>
2022-12-22 11:17:43 -0300

    java syntax tutorial

---
+++ core-java/java-syntax/src/main/java/br/com/sitetune/basicsyntax/SampleAddition.java
@@ -0,0 +1,11 @@
+package br.com.sitetune.basicsyntax;
+
+public class SampleAddition {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        double z = x + y;
+        System.out.println(x + " + " + y + " = " + z);
+    }
+}
